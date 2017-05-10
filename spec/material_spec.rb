d704ab80df256ad6397ee42fc9cb3542235099d7
Shereen <shereenfinney@gmail.com>
2017-05-10 16:23:00 +0100

    UPDATE MATERIAL SPEC
    Create :material as described subject
    Add describe, context and it blocks
    Unit test to check Material is initialised with a unique clock id

--- spec/material_spec.rb
+++ spec/material_spec.rb
@@ -1,5 +1,15 @@
 require "Material"
 
 describe Material do
+  subject(:material) {described_class.new}
 
+  describe "#initialize" do
+    context "when created" do
+      it "has a clock number id" do
+        material = Material.new('WNP/SWCL001/010')
+        expect { (material.id).to eq('WNP/SWCL001/010') }
+      end
+    end
+  end
+  
 end
