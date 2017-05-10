a16d82b48af9d15e36ef61a72bd68ac79de03cac
Shereen <shereenfinney@gmail.com>
2017-05-10 13:38:27 +0100

    UPDATE SPEC/BROADCASTER_SPEC.RB
    Create :broadcaster as described subject
    Expect broadcaster.id to not error

--- spec/broadcaster_spec.rb
+++ spec/broadcaster_spec.rb
@@ -1,5 +1,14 @@
 require "Broadcaster"
 
 describe Broadcaster do
+  subject(:broadcaster) { described_class.new}
+
+  describe "#initialize" do
+    context "when created" do
+      it "has an ID" do
+        expect {broadcaster.id}.not_to raise error
+      end
+    end
+  end
 
 end
