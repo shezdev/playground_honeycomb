b8b66c8487f60536da38f4af90573d077d55be51
Shereen <shereenfinney@gmail.com>
2017-05-10 15:18:41 +0100

    UPDATE DELIVERY SPEC Create :delivery as described subject Add describe, context and it blocks Unit test to check Delivery is initialised with a name and price

--- spec/delivery_spec.rb
+++ spec/delivery_spec.rb
@@ -1,5 +1,16 @@
 require "Delivery"
-describe Delivery do
 
+describe Delivery do
+  subject(:delivery) {described_class.new}
+
+  describe "#initialize" do
+    context "when created" do
+      it "has a name and a price" do
+        standard_delivery = Delivery.new(:standard, 10.0)
+        expect { (standard_delivery.name).to eq(:standard) }
+        expect { (standard_delivery.price).to eq(10.0) }
+      end
+    end
+  end
 
 end
